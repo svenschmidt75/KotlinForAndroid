a57c9decd18447961dd1b1bd4629562d488a01db
Sven <svenschmidt75@googlemail.com>
2019-11-28 08:03:29 -0700

    more layout changes

--- app/src/main/java/com/example/habittracker/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.habittracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iv_icon.setImageResource(R.drawable.water)
+        tv_title.text = "Title"
+        tv_description.text = "A refreshing glass"
     }
 }
