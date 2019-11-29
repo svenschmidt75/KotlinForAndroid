4915e36c2786ee23c3dfca670d785ea1d1cde48f
Sven <svenschmidt75@googlemail.com>
2019-11-29 09:38:33 -0700

    wire up create new habit activity

--- app/src/main/java/com/example/habittracker/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.habittracker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,4 +25,17 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_habit) {
+            switchTo(CreateHabitActivity::class.java)
+        }
+        return true
+    }
+
+    private fun switchTo(c: Class<*>) {
+        val intent = Intent(this, c)
+        startActivity(intent)
+    }
+
 }
