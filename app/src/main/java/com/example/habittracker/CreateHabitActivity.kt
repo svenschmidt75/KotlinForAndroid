9c72cfbc2dc55f6054ad41934e2b682696c66bda
Sven <svenschmidt75@googlemail.com>
2019-11-29 10:29:22 -0700

    set image in UI after select

--- app/src/main/java/com/example/habittracker/CreateHabitActivity.kt
+++ app/src/main/java/com/example/habittracker/CreateHabitActivity.kt
@@ -1,14 +1,22 @@
 package com.example.habittracker
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_habit.*
+import java.io.IOException
 
 class CreateHabitActivity : AppCompatActivity() {
 
     private val TAG = CreateHabitActivity::class.simpleName
+    private val CHOOSE_IMAGE_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +29,35 @@ class CreateHabitActivity : AppCompatActivity() {
         intent.action = Intent.ACTION_GET_CONTENT
 
         val chooser = Intent.createChooser(intent, "Choose image for habit")
-        startActivity(chooser)
+        startActivityForResult(chooser, CHOOSE_IMAGE_REQUEST)
 
         Log.d(TAG, "Intent to choose image sent...")
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CHOOSE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            Log.d(TAG, "An image was chosen by the user.")
+
+            val bitmap = tryReadBitmap(data.data!!)
+
+            bitmap?.let {
+                iv_image.setImageBitmap(bitmap)
+                Log.d(TAG, "Read image and updated image view.")
+            }
+        }
+    }
+
+    private fun tryReadBitmap(data: Uri): Bitmap? {
+        return try {
+            MediaStore.Images.Media.getBitmap(contentResolver, data)
+        }
+        catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+
 }
