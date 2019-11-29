4539d662dc75fcfba2a4a0ba5d901880ca8f0cb5
Sven <svenschmidt75@googlemail.com>
2019-11-29 10:13:14 -0700

    select image for new activity

--- app/src/main/java/com/example/habittracker/CreateHabitActivity.kt
+++ app/src/main/java/com/example/habittracker/CreateHabitActivity.kt
@@ -1,12 +1,28 @@
 package com.example.habittracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class CreateHabitActivity : AppCompatActivity() {
+
+    private val TAG = CreateHabitActivity::class.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_habit)
+    }
+
+    fun chooseImage(view: View) {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        val chooser = Intent.createChooser(intent, "Choose image for habit")
+        startActivity(chooser)
+
+        Log.d(TAG, "Intent to choose image sent...")
     }
 }
